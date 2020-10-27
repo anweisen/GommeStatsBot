f5f9baffdd6ebfdd36c58f7d1d74b836982ee63f
anweisen <anweisen@web.de>
2020-10-27 22:19:06 +0100

    Trying reconnect on time out

--- src/main/java/net/anweisen/gommestats/manager/stats/StatsWrapper.java
+++ src/main/java/net/anweisen/gommestats/manager/stats/StatsWrapper.java
@@ -6,7 +6,9 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import javax.annotation.Nonnull;
 import java.io.IOException;
+import java.net.SocketTimeoutException;
 
 /**
  * @author anweisen
@@ -16,18 +18,61 @@ import java.io.IOException;
 
 public class StatsWrapper {
 
-	public static PlayerStats getsStatsByNameWithException(String playerName, GameMode gamemode) throws IOException, StatsHiddenException {
+	public static PlayerStats getsStatsByName(String playerName, GameMode gamemode) throws IOException, StatsHiddenException {
 
-		String request = PlayerStats.getURL(playerName, gamemode);
-		Document document = ConnectionManager.openConnection(request);
+		Document document = getConnection(playerName, 0);
 
-		Element element = document.getElementById(gamemode.getHTMLName());
-		if (element == null) throw new StatsHiddenException();
+		Element gameModeElement = document.getElementById(gamemode.getHTMLName());
+		if (gameModeElement == null)
+			throw new StatsHiddenException();
 
-		Elements elements = element.getElementsByClass("score");
-		if (elements == null || elements.isEmpty()) throw new StatsHiddenException();
+		Elements scores = gameModeElement.getElementsByClass("score");
+		if (scores == null || scores.isEmpty())
+			throw new StatsHiddenException();
 
-		return gamemode.instance(elements);
+		return gamemode.instance(scores);
+
+	}
+
+	public static PlayerStats[] getsStatsByName(String playerName, GameMode... gamemodes) throws IOException, StatsHiddenException {
+
+		Document document = getConnection(playerName, 0);
+
+		PlayerStats[] stats = new PlayerStats[gamemodes.length];
+		for (int i = 0; i < stats.length; i++) {
+
+			GameMode gamemode = gamemodes[i];
+
+			Element gameModeElement = document.getElementById(gamemode.getHTMLName());
+			if (gameModeElement == null)
+				continue;
+
+			Elements scores = gameModeElement.getElementsByClass("score");
+			if (scores == null || scores.isEmpty())
+				continue;
+
+			stats[i] = gamemode.instance(scores);
+
+		}
+
+		return stats;
+
+	}
+
+	public static Document getConnection(@Nonnull String playerName, int cycle) throws IOException {
+
+		// Simple retry system
+		// Retries on timeout up to 3 times
+		try {
+			String request = PlayerStats.getURL(playerName, null);
+			return ConnectionManager.openConnection(request, 500);
+		} catch (SocketTimeoutException ex) {
+			if (cycle < 3) {
+				return getConnection(playerName, ++cycle);
+			} else {
+				throw ex;
+			}
+		}
 
 	}
 
